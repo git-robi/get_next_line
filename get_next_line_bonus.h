b846efcd99b159d9f311229319183e7334f23e6e
Roberta Giambona <nemo@Robertas-MacBook-Pro.local>
2024-08-06 09:55:48 +0200

    feat: add bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,8 +1,9 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
@@ -17,7 +18,7 @@ typedef struct s_list
 
 void	fill_new_list(t_list *tmp, t_list *new_list);
 char	*get_next_line(int fd);
-void	read_and_putnode(int fd, t_list **list);
+int	read_and_putnode(int fd, t_list **list);
 int		new_line(t_list **list);
 t_list	*add_new_node(t_list **list, int is_new_list);
 void	reset_list(t_list **list);
